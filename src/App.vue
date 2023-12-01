3ac857888ba19378705ea14a697c3536a8f2326a
Amanali Abilkhan <a.abilkhan024@gmail.com>
2023-12-01 20:58:58 +0600

    feat(project-setup): upgrade to vue 2.7+ to allow composition api

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,17 @@
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  setup() {
+    // alert("asd");
+  },
+});
+</script>
+
 <template>
-  <div id="app">
-    <nav>
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </nav>
-    <router-view/>
-  </div>
+  <nav>
+    <router-link to="/">Home</router-link> |
+    <router-link to="/about">About</router-link>
+    <!-- <router-view /> -->
+  </nav>
 </template>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-nav {
-  padding: 30px;
-}
-
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-nav a.router-link-exact-active {
-  color: #42b983;
-}
-</style>
--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,13 @@
+import { fileURLToPath, URL } from "node:url";
+import vue from "@vitejs/plugin-vue2";
 import { defineConfig } from "vite";
-import { createVuePlugin as vue } from "vite-plugin-vue2";
-import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
   resolve: {
-    extensions: [".mjs", ".js", ".ts", ".jsx", ".tsx", ".json", ".vue"],
     alias: {
-      "@": path.resolve(__dirname, "./src"),
+      "@": fileURLToPath(new URL("./src", import.meta.url)),
     },
   },
 });
